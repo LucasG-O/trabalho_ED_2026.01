f4f38945a887c7962b11663bfab79ac33643b448
unknown <gaboliveira.lucas@gmail.com>
2026-03-26 13:33:52 -0300

    batata

---
+++ ReservationRequest.cpp
@@ -0,0 +1,1 @@
+#include "ReservationRequest.hpp"
---
+++ ReservationSystem.hpp
@@ -0,0 +1,30 @@
+#include <string>
+#include "ReservationRequest.hpp"
+
+
+class ReservationSystem {
+
+private:
+    int room_count;
+    int* room_capacities;
+    struct ReservaNode {
+    ReservationRequest requisicao;
+    ReservaNode* proximo;
+};
+
+    // Estruturas internas escolhidas pelos alunos
+    // para armazenar e gerenciar as reservas, os horários, ...
+
+public:
+
+    ReservationSystem(int room_count, int* room_capacities);
+    ~ReservationSystem();
+
+    bool reserve(ReservationRequest request);
+    bool cancel(std::string course_name);
+
+    void printSchedule();
+
+    // Outros métodos utilitários necessários
+    // para auxiliar nas funções requisitadas
+};
